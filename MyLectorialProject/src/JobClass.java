011788bd27c1da46d09c7354a36f2d085ecccb74
E116324 <E116324@localhost>
2023-05-18 14:59:03 +1000

    first commit

---
+++ MyLectorialProject/src/JobClass.java
@@ -0,0 +1,15 @@
+
+public class JobClass {
+
+	private int jobID;
+	private String jobTitle;
+	public void updateJobTitle()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
